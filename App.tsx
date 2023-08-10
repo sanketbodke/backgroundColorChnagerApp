2dbbe0ae5c96d72ea73fb4463428f8c23042348a
sanket bodke <sanketbodke26@gmail.com>
2023-08-10 20:11:55 +0530

    working on shapes

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react'
 export default function App() {
 
   const [backgroundColor, setBackgroundColor] = useState("#ffffff")
+  const [boxColor, setBoxColor] = useState("#ffffff")
 
   const generateColor = ()=>{
     const hexRange = "0123456789ABCDEF"
@@ -23,6 +24,18 @@ export default function App() {
       <View style={styles.actionBtn}>
         <Text style={styles.actionBtnText}>Press Me</Text>
       </View>
+      <View style={[styles.box1, {backgroundColor: boxColor}]}>
+
+      </View>
+      <View style={[styles.box2, {backgroundColor: boxColor}]}>
+
+      </View>
+      <View style={[styles.box3, {backgroundColor: boxColor}]}>
+
+      </View>
+      <View style={[styles.box4, {backgroundColor: boxColor}]}>
+   
+      </View>
     </TouchableOpacity>
    </View>
    </>
@@ -40,11 +53,47 @@ const styles = StyleSheet.create({
     borderRadius: 12,
     backgroundColor: "#6A1B4D",
     padding: 13,
+    position: "relative"
   },
 
   actionBtnText:{
     color: "#FFF",
     fontSize: 18,
     textTransform: "uppercase"
+  },
+
+  box1:{
+    position: "absolute",
+    top: 250,
+    height: 70,
+    width: 100,
+    // backgroundColor: "maroon"
+  },
+
+  box2:{
+    position: "absolute",
+    top: -250,
+    height: 70,
+    width: 100,
+    borderRadius: 20,
+    // backgroundColor: "blue"
+  },
+
+  box3:{
+    position: "absolute",
+    top: 110,
+    height: 100,
+    width: 100,
+    borderRadius: 50,
+    // backgroundColor: "pink"
+  },
+
+  box4:{
+    position: "absolute",
+    top: -150,
+    height: 70,
+    width: 100,
+    borderRadius: 50,
+    // backgroundColor: "yellow"
   }
 })
